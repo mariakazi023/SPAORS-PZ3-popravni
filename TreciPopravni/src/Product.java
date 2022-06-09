2b34e380bc1207439cd837c738334518c0c85d64
mariakazi023 <kazimaria023@gmail.com>
2022-06-09 15:57:40 +0200

    Ispravljeni bug-ovi

--- TreciPopravni/src/Product.java
+++ TreciPopravni/src/Product.java
@@ -1,14 +1,10 @@
 
 public class Product {
-	
+
 	
 	
 	public int product(int n) {
-<<<<<<< HEAD
 		int result = 0;
-=======
-		int result = 1;
->>>>>>> maria-kazi
 		for(int i = 1; i < n; i++) {
 			result *= i;
 		}
